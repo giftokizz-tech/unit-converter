9376af7aee0a56df28786528d6676fc65b2afebf
giftokizz-tech <giftokizz@gmail.com>
2026-06-11 12:05:24 +0100

    created unit conveter

---
+++ index.js
@@ -0,0 +1,73 @@
+/* 
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+let inputEl = document.getElementById("input-el")
+const convertBtn = document.getElementById("covert-btn")
+const meterFeetEl = document.getElementById("meters-feet-el")
+const litterGallonEl = document.getElementById("litters-gallons-el")
+const kiloPoundsEl = document.getElementById("kilos-pounds-el")
+
+
+convertBtn.addEventListener("click", function(){
+    let num = Number(inputEl.value)
+    lengthm()
+    volume()
+    mass()
+    inputEl.focus()
+    
+})
+
+function lengthm() {
+    let num = Number(inputEl.value)
+    let feet = num * 3.281
+    let meter = num / 3.281
+    let lengthmResult = `
+    ${num} meters = ${feet.toFixed(2)} feet | ${num} feet = ${meter.toFixed(2)} meters
+    `
+    if(num < 2) {
+     meterFeetEl.textContent =   `
+    ${num} meter = ${feet.toFixed(2)} feet | ${num} foot = ${meter.toFixed(2)} meters
+    `
+    } else {
+        meterFeetEl.textContent = lengthmResult
+    }
+}
+lengthm()
+
+
+function volume() {
+    let num = Number(inputEl.value)
+    let liters = num * 3.785
+    let gallons = num / 3.785
+    let volumeResult = `
+    ${num} liters = ${gallons.toFixed(2)} gallons | ${num} gallons = ${liters.toFixed(2)} liters
+    `
+    if (num < 2) {
+        litterGallonEl.textContent =  `
+    ${num} liter = ${gallons.toFixed(2)} gallons | ${num} gallon = ${liters.toFixed(2)} liters
+    `
+    } else {
+         litterGallonEl.textContent = volumeResult
+    }
+}
+volume()
+
+function mass() {
+    let num = Number(inputEl.value)
+    let kilo = num / 2.204
+    let pounds = num * 2.204
+    let massResult = `
+        ${num} kilos = ${pounds.toFixed(2)} pounds | ${num} pounds = ${kilo.toFixed(2)} kilos
+        `
+        if (num < 2) {
+            kiloPoundsEl.textContent = `
+        ${num} kilo = ${pounds.toFixed(2)} pounds | ${num} pound = ${kilo.toFixed(2)} kilos
+        `
+        } else {
+            kiloPoundsEl.textContent = massResult
+        }
+    }
+    mass()
